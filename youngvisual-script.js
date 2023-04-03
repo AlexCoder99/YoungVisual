a71b4b449291e283ea263d30f12e0b76d1fa71b8
AlexCoder99 <88981534+AlexCoder99@users.noreply.github.com>
2023-04-03 17:59:16 +0200

    Update youngvisual-script.js

--- youngvisual-script.js
+++ youngvisual-script.js
@@ -1,8 +1,31 @@
+// slide down navbar on scroll
+
+let navHeader = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        navHeader.style.top = "0";
+      } else {
+        navHeader.style.top = "-80px";
+      }
+})
+
+
+// marketing quotes footer (fetch request)
+
 const marketingQuotes = fetch('marketing-quotes.json');
-marketingQuotes.then(response => response.json())
+marketingQuotes.then(response => {
+  if(!response.ok){
+    throw new Error(`HTTP error - ${response.status}`);
+  }
+  return response.json();
+})
 .then(data => {
     const sameItem = Math.floor(Math.random() * data["authors"].length);
-    console.log(sameItem);
     document.getElementById("A").innerHTML = data["quotes"][sameItem];
     document.getElementById("B").innerHTML = data["authors"][sameItem]; 
+})
+.catch(error => {
+  console.error(`Fetch request failed, cannot load footer quotes. ${error} `)
 });
+
